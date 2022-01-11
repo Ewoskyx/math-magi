5fb6271d4cba6acabb31835386f51c2150dd3405
Ewoskyx <ewoskyx@gmail.com>
2022-01-11 18:33:22 +0300

    Update deploy for route

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
